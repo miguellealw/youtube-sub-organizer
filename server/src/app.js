44c2ba4ec695fed965ebf04cb3c8d27dc8c65b19
Miguel <miguellealwebdev@gmail.com>
2018-08-01 00:11:49 -0500

    Init Commit

---
+++ server/src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const middleware = require('../config/middleware');
+import routes from './api';
+
+const app = express();
+
+// middleware(app);
+// routes(app);
+
+app.listen(3000, function(err) {
+  if(err) {
+    console.log(`
+      =============
+      Error Starting Server
+      ${err}
+      ============
+    `)
+  }
+  console.log(`
+    ============
+    Server Running on http://localhost:3000
+    ============
+  `)
+});
