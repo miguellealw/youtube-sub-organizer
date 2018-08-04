cb25e7b522205267a85acd64a926ddd44c61d742
Miguel <miguellealwebdev@gmail.com>
2018-08-04 00:15:57 -0500

    changed google oauth to youtube oauth

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -2,17 +2,22 @@ import passport from 'passport';
 import keys from '../config/keys';
 
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const YoutubeV3Strategy = require('passport-youtube-v3').Strategy
 
 module.exports = (app) => {
 
   // Setup google strategy
   passport.use(
-    new GoogleStrategy({
+    new YoutubeV3Strategy({
       clientID: keys.youtubeClientID,
       clientSecret: keys.youtubeClientSecret,
-      callbackURL: '/auth/google/callback'
-    }, (accessToken) => {
-      console.log(accessToken);
+      callbackURL: '/auth/youtube/callback',
+      scope: ['https://www.googleapis.com/auth/youtube.readonly']
+    }, (accessToken, refreshToken, profile, done) => {
+      // Refresh token used to automatically update accessToken when it expires
+      console.log('accessToken', accessToken);
+      console.log('refreshToken', refreshToken);
+      console.log('profile', profile);
     }), 
   );
 }
--- server/src/api/index.js
+++ server/src/api/index.js
@@ -2,14 +2,22 @@
 // import categoryRoutes from './category/categoryRoutes';
 import passport from 'passport';
 
-export default app => {
+export default (app) => {
   // app.use('/', (req, res) => res.send('hello worldsss'));
   // app.use('/api/v1/category', categoryRoutes);
   // app.use('/api/v1/subscriptions', subsRoutes);
   app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-      scope: ['profile', 'email']
-    })
+    '/auth/youtube',
+    passport.authenticate('youtube')
   )
+
+  app.get(
+    '/auth/youtube/callback',
+    passport.authenticate('youtube')
+  )
+}
+
+
+export const setupModels = (Sequelize, connection, models) => {
+  models.map(model => model(Sequelize, connection))
 }
